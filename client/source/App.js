bf9e67df5a36076b5848199c6dc732a9c562f83a
unknown <dyomin.dyominalexander2011@yandex.ru>
2020-10-09 12:14:28 +0300

    Client: Conditional 'in-dev|in-pro' App

--- client/source/App.js
+++ client/source/App.js
@@ -5,11 +5,17 @@ import './App.scss';
 
 import { Welcome, Home } from './components/';
 
+try {
+    var is_in_dev = require('../fake_data/fake.json');
+} catch (err) { }
+
 const App = () => {
     const [stage, setStage] = useState('logging');
 
     let content = <Welcome onLogin={_ => setStage('logged')}></Welcome>;
-    //let content = <Home></Home>;
+    if (is_in_dev !== undefined) {
+        content = <Home></Home>;
+    }
     if (stage === 'logged') {
         content = <Home></Home>
     }
