739a8bb9082608804eeac0d35d12974ca0d85225
unknown <dyomin.dyominalexander2011@yandex.ru>
2020-10-10 17:22:07 +0300

    Client: Working CreateRoom screen

--- client/source/components/creation_room/CreationRoom.js
+++ client/source/components/creation_room/CreationRoom.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Button } from '../index';
 import { TextField } from '@material-ui/core';
 
+import Socket from '../../Socket';
+
 import './CreationRoom.scss';
 
 const CreationRoom = ({onComplete}) => {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    /*
+        Пока joinroom всегда возвращает success. Если это изменится, то нужно будет хендлить ошибки здесь
+    */
+
+    const onCreateBtn = () => {
+        Socket.emit('joinroom', {
+            name: name,
+            password: (password.length === 0) ? null : password
+        });
+        onComplete();
+    }
+
     return (
         <div className="creation-room">
             <div className="creation-room__bg"></div>
             <div className="creation-room__content">
                 <h2 className="creation-room__header">Create room</h2>
-                <TextField className="creation-room__input" placeholder="Room name"></TextField>
-                <TextField className="creation-room__input" placeholder="Room password"></TextField>
+                <TextField onChange={e => setName(e.target.value)} className="creation-room__input" placeholder="Room name"></TextField>
+                <TextField onChange={e => setPassword(e.target.value)} className="creation-room__input" placeholder="Room password"></TextField>
                 <div className="creation-room__buttons">
                     <Button onClick={onComplete}>Cancel</Button>
-                    <Button onClick={onComplete}>Create</Button>
+                    <Button onClick={onCreateBtn}>Create</Button>
                 </div>
             </div>
         </div>
